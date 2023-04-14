6ec477aef6e2b8d1730fd4ffb282f84e3c5b062f
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-14 22:53:45 +0000

    add the timer functionality

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,10 +1,37 @@
 import "./Footer.css"
 import SquareHr from "../Squares/Squares"
+import { useState, useEffect } from "react"
 
 function Footer(prop) {
+  const [time, setTime] = useState(0);
+  const [timerId, setTimerId] = useState(null);
+
+
+  useEffect(() => {
+      const id = setInterval(() => {
+        setTime(prevTime => prevTime + 1 )
+      }, 1000);
+      
+      setTimerId(id)
+
+      return () => clearInterval(id)
+  }, [])
+
+  const timer = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    return `${minutes.toString().padStart(2, '0')} : ${seconds.toString().padStart(2, '0')}`
+
+  }
+
+  // const handelStopTimer = () => {
+  //     clearInterval(timerId)
+  // }
+
   return (
     <div className="footer">
-        <SquareHr text={"1:53"} title={"Time"}/>
+        <SquareHr text={timer(time)} title={"Time"}/>
         <SquareHr text={prop.moves} title={"Moves"}/>
     </div>
   )
